3b7210141b9e2e22b11aab51fa8f9b4998bc200a
abhirajchatterjee <chatterjeeabhirajofficial@gmail.com>
2025-01-03 01:06:03 +0530

    rectified typo

--- packages/shared/lib/src/task_repo.dart
+++ packages/shared/lib/src/task_repo.dart
@@ -4,6 +4,6 @@ abstract interface class TaskRepo{
   Future<List<Task>> fetchAllTasks();
   List<Task> getTaskById(String id);
   Future<Task> addTask(Task task);
-  Future<Task> uodateTask(Task task);
+  Future<Task> updateTask(Task task);
   Future<Task> deleteTask(Task task);
 }
